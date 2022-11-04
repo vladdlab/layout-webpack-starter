8b139869efc31d05cb358d60d9090f198a638cde
Бочманов Владислав <vladislavbochmanov@gmail.com>
2022-11-04 17:41:29 +0300

    feat: add basic loaders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = "Hello, me!"
+import './style.css';
+import Icon from './assets/frankenstein.png';
 
-  return element;
+function createApp() {
+  const app = document.getElementById('app');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  const p = document.createElement('p');
+  p.textContent = 'Hello, me!';
+
+  app.appendChild(myIcon);
+  app.appendChild(p);
 }
 
-document.body.appendChild(component());
+createApp();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
